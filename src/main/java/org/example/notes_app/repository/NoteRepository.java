7612966a56a942f22a1f7255a0c64e9990bd3094
AdamMudrak <mudrets2000@gmail.com>
2026-02-18 22:09:39 +0200

    Use Query (spring not enough)

--- src/main/java/org/example/notes_app/repository/NoteRepository.java
+++ src/main/java/org/example/notes_app/repository/NoteRepository.java
@@ -4,6 +4,7 @@ import org.example.notes_app.entity.Note;
 import org.example.notes_app.entity.Tag;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.data.mongodb.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
@@ -12,9 +13,10 @@ import java.util.Set;
 
 @Repository
 public interface NoteRepository extends MongoRepository<Note, String> {
-    List<Note> findAllByUsernameAndTagsIn(String username, Set<Tag> tags, Pageable pageable);
+    @Query("{ 'username': ?0, 'tags': { $in: ?1 } }")
+    List<Note> findAllByUsernameAndTagsContainsAny(String username, Set<Tag> tags, Pageable pageable);
 
-    List<Note> findAllByUsername(String username);
+    List<Note> findAllByUsername(String username, Pageable pageable);
 
     Optional<Note> findByUsernameAndId(String username, String id);
 }
