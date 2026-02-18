b57dbb2656ed9c826a4906769eb0223c68a12757
AdamMudrak <mudrets2000@gmail.com>
2026-02-18 22:11:54 +0200

    Point at ui html as well, delete unnecessary public

--- src/main/java/org/example/notes_app/config/SecurityConfig.java
+++ src/main/java/org/example/notes_app/config/SecurityConfig.java
@@ -22,7 +22,7 @@ public class SecurityConfig {
                 .csrf(AbstractHttpConfigurer::disable)
                 .authorizeHttpRequests(auth -> auth
                         .requestMatchers(
-                                "/public/**",
+                                "/swagger-ui.html",
                                 "/swagger-ui/**",
                                 "/v3/api-docs/**"
                         ).permitAll()
